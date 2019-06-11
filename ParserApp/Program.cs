8462f01aea9b971d575e1ddb1e9bc019c9fc2c47
Roma <31734788+romaGri@users.noreply.github.com>
2019-06-11 17:51:38 +0300

    Add files via upload

---
+++ ParserApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Library;
+
+namespace ParserApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Parser(10000).Parse(@"D:\ProjectTrash\1.xml");
+            new Parser(10000).Parse(@"C:\Users\Raman_Hrytsuk\Downloads\rutracker-20190323.xml.gz");
+        }
+    }
+}
