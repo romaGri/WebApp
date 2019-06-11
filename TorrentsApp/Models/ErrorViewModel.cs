41c2894336c5cc7444ca8dc02878e76698d47cff
Roma <31734788+romaGri@users.noreply.github.com>
2019-06-11 17:54:48 +0300

    Add files via upload

---
+++ TorrentsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TorrentsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
